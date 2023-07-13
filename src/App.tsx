0346a7fe4bf4d31c763df699fea21cdc91e1bdf1
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-07-12 22:38:44 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar.tsx'
+import Landing from './components/Landing.tsx'
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  )
+}
+
+export default App
