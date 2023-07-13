89a4d2f485520b4538cc10321153558d09046468
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-07-13 10:51:22 -0400

    Contact component
    added contact component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from './components/Navbar.tsx'
 import Landing from './components/Landing.tsx'
+import Contact from './components/Contact.tsx'
 
 const App = () => {
 
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Navbar />
       <Landing />
+      <Contact />
     </>
   )
 }
