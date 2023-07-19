02216daf22ce3493951545fc2fa36c3a04d37ac1
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2023-07-18 22:15:54 -0400

    deployment issues III

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import Navbar from './components/Navbar.tsx'
 import Landing from './components/Landing.tsx'
 import Contact from './components/Contact.tsx'
 import Speciality from './components/Speciality.tsx'
-import Project from './components/Project.tsx'
 import Carousel from './components/Carousel.tsx'
-import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
 
@@ -14,7 +12,6 @@ const App = () => {
       <Navbar />
       <Landing />
       <Speciality />
-      {/* <Project /> */}
       <br />
       <Carousel />
       <Contact />
